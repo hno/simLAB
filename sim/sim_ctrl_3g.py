b36841455f00328fc08ff5c7d69eba17a3e546b3
Henrik Nordström <henrik.nordstrom@borderlight.net>
2018-09-17 17:06:49 +0200

    Allow PIN codes to be specified as 16 HEX digits (8 octets)
    Some vendors use binary adm pin codes, specifying the adm code
    in hex notation.

--- sim/sim_ctrl_3g.py
+++ sim/sim_ctrl_3g.py
@@ -1095,7 +1095,10 @@ class SimCtrl(object):
         if not code:
             code = sim_codes.defaultCard[pinId]
         #VERIFY
-        code = types.addTrailingBytes(code.encode("hex"), 0xFF, 8)
+        if len(code) == 16:
+            code = code.upper()
+        else:
+            code = types.addTrailingBytes(code.encode("hex"), 0xFF, 8)
         sw1, sw2, data = self.sendApdu("00%02X00%02X%02X%s%s" %(verify, chvP2, length, code, newCode))
 
         sw1Name, swName = types.swName(sw1, sw2)
